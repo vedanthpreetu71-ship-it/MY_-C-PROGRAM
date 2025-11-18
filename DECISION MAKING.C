a503aff45429348cabc2229ae14a3c04fcaa269d
vedanthpreetu71-ship-it <vedanthpreetu71@gmail.com>
2025-11-18 15:52:03 +0530

    Create DECISION MAKING.C

---
+++ DECISION MAKING.C
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int jaybalaya(){
+    
+    printf(" my name is\n");
+     printf(" my age is\n");
+   printf("my department name is\n");
+}
+int main(){
+    jaybalaya();
+    return 0;
+}
