8680d2b557d486ec5377c6c3edee357383d26103
vedanthpreetu71-ship-it <vedanthpreetu71@gmail.com>
2025-11-18 12:17:08 +0530

    Create FIBONACCI    SERIES .C

---
+++ FIBONACCI    SERIES .C
@@ -0,0 +1,19 @@
+ 
+
+
+#include <stdio.h>
+
+int main() {
+    int n, a = 0, b = 1, next, i;
+
+    printf("Enter how many terms: ");
+    scanf("%d", &n);
+
+    printf("Fibonacci Series:\n");
+
+    for (i = 1; i <= n; i++) {
+        printf("%d ", a);
+        next = a + b;  
+        a = b;          
+        b = next;       
+    }
