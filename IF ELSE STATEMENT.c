f8c66d6f002806ee2510da0a8445cfbdd32a8413
vedanthpreetu71-ship-it <vedanthpreetu71@gmail.com>
2025-11-18 12:04:59 +0530

    Create IF ELSE STATEMENT.c

---
+++ IF ELSE STATEMENT.c
@@ -0,0 +1,21 @@
+
+
+
+
+#include <stdio.h>
+
+int main() {
+    int age ;
+    printf("enter the age\n");
+    scanf("%d",&age);
+    if(age>=18)
+    {
+    printf("you are eligible to take driving license\n");
+    }
+    else {
+        printf("you are not a eligible ");
+    
+    }
+    
+}
+
