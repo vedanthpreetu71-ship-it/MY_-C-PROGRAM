0a89fcb13c7ae93b0f7fac1b6acd418cd7968413
vedanthpreetu71-ship-it <vedanthpreetu71@gmail.com>
2025-11-20 14:01:37 +0530

    Implement string copy functionality in cpy.c

---
+++ STRING cpy.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include<string.h>
+int main() {
+    char str[50],rev[50];
+    printf("enter a string:");
+    scanf("%s",str);
+    strcpy(rev,str);
+    printf("%s\n",rev);
+    
+}
