21f2ccd47f007d3591acd09136c47d3db723b82b
vedanthpreetu71-ship-it <vedanthpreetu71@gmail.com>
2025-11-20 19:14:29 +0530

    Create  DECREMENT ARRAY  ELEMENTS.C

---
+++ DECREMENT ARRAY  ELEMENTS.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+    int a[5], i;
+     printf("Enter 5 elements:\n");
+    for(i = 0; i < 5; i++) {
+        scanf("%d", &a[i]);
+    }
+       for(i = 0; i < 5; i++) {
+        a[i] = a[i] - 1;
+    }
+
+    printf("Array after decrement:\n");
+    for(i = 0; i < 5; i++) {
+      printf("%d ", a[i]);
+    }
+
+}
