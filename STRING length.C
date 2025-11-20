60ce5cb646586cbc95e4068460cc8b50066d2f38
vedanthpreetu71-ship-it <vedanthpreetu71@gmail.com>
2025-11-20 14:03:26 +0530

    Add string length and comparison functionality
    Implement a program to read a string, copy it, and compare lengths.

---
+++ STRING length.C
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include<string.h>
+int main() {
+    char str[50],rev[50];
+    printf("enter a string:");
+    scanf("%s",str);
+    strcpy(rev,str);
+    printf("%s\n",rev);
+    int a=strcmp(str,rev);
+    printf("%d",a);
+    int length=strlen(str);
+    printf("%d",length);
+}
