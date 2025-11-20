5d7cb80ca0e5f71ec08711902ec3b0b2d50abaf4
vedanthpreetu71-ship-it <vedanthpreetu71@gmail.com>
2025-11-20 14:00:54 +0530

    Add palindrome checking program in C

---
+++ POLINDROME OR NOT.C
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+    int len, i, flag = 0;
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    len = strlen(str);
+
+    for (i = 0; i < len / 2; i++) {
+        if (str[i] != str[len - i - 1]) {
+            flag = 1;
+            break;
+        }
+    }
+
+    if (flag == 0)
+        printf("The string is a palindrome.\n");
+    else
+        printf("The string is not a palindrome.\n");
+}
+
